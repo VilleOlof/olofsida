cce4324d56cd4caadcd9114a69b8ef0c9dfb2759
Ville <villeolofsgard@gmail.com>
2023-04-01 02:21:16 +0200

    early stuff

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<footer>
+    <p>hello</p>
+</footer>
+
+<style>
+    footer {
+        background: #222;
+        padding: 1em;
+        text-align: center;
+        color:white;
+    }
+</style>
+
+<slot></slot>
