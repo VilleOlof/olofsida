854f3926615607d2c10c3d1fd434abe83fc2bdf5
VilleOlof <villeolof@gmail.com>
2023-10-05 13:15:49 +0200

    npm audit fix and some redirect changes

---
+++ src/routes/qr/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import Redirect from '../../lib/Redirect.svelte';
+
+    const URL: string = 'https://www.youtube.com/@Olof?sub_confirmation=1';
+
+    goto(URL);
+</script>
+
+<Redirect url={URL}/>
